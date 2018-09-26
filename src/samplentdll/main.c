c18755a6bf2f2a72c57a3fe78fe16cd52afc9117
Petr Benes <petr.benes@avast.com>
2018-09-26 17:15:27 +0200

    update

--- src/samplentdll/main.c
+++ src/samplentdll/main.c
@@ -12,15 +12,19 @@ NtDllMain(
   switch (Reason)
   {
     case DLL_PROCESS_ATTACH:
+
       break;
 
     case DLL_PROCESS_DETACH:
+
       break;
 
     case DLL_THREAD_ATTACH:
+
       break;
 
     case DLL_THREAD_DETACH:
+
       break;
   }
 
