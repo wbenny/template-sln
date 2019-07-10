b6a37258513402ce2ba1f75fae663589a4142b2b
Petr Benes <w.benny@outlook.com>
2019-07-10 16:26:02 +0200

    update

---
+++ src/samplemin/main.c
@@ -0,0 +1,7 @@
+#include <ntdll.h>
+#include <windows.h>
+
+void MinMain()
+{
+  MessageBox(0, TEXT("TEXT"), TEXT("CAPTION"), 0);
+}
---
+++ src/samplemindll/main.c
@@ -0,0 +1,31 @@
+#include <ntdll.h>
+
+BOOL
+NTAPI
+MinDllMain(
+  _In_ HANDLE ModuleHandle,
+  _In_ ULONG Reason,
+  _In_ LPVOID Reserved
+  )
+{
+  switch (Reason)
+  {
+    case DLL_PROCESS_ATTACH:
+
+      break;
+
+    case DLL_PROCESS_DETACH:
+
+      break;
+
+    case DLL_THREAD_ATTACH:
+
+      break;
+
+    case DLL_THREAD_DETACH:
+
+      break;
+  }
+
+  return TRUE;
+}
